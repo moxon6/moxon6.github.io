58e4dd98d886beb58f626bcb95a4052c42c2c216
Martin Moxon <moxon6@googlemail.com>
2019-12-27 11:41:56 +0000

    Updates

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This branch is used only as github pages deployment target 
+This branch is used only as github pages deployment target
---
+++ precache-manifest.39f9dc09e471f68b85b376391869d49c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04b0c1255a0f807aa546d67019bd17c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d832711b96eee0b236f",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3adfe8d603e0d9213ca8",
+    "url": "/static/js/2.150e8e2a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.150e8e2a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2d832711b96eee0b236f",
+    "url": "/static/js/main.f465ecf7.chunk.js"
+  },
+  {
+    "revision": "a1689bd35492f0922289",
+    "url": "/static/js/runtime-main.d6ace94e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
