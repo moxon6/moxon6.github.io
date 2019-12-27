d8b12919bfdc2ab74baa5626d71a3a70274397a2
Martin Moxon <moxon6@googlemail.com>
2019-12-27 19:53:06 +0000

    Terminal start example

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,62 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import './App.scss';
 import { getProjects } from './api/github';
+import background from './background.png';
+import background2 from './background2.jpg';
 
-import fallback from './placeholder.jpg';
-import octocat from './github.png';
+
+document.addEventListener('keyup', (e) => {
+  if (e.code === "ArrowUp") window.decrement()
+  else if (e.code === "ArrowDown") window.increment()
+});
 
 export default function App() {
 
-  const [projects, setProjects] = useState([])
+  const [_projects, setProjects] = useState([])
+  const [selected, setSelected] = useState(2)
+  window.increment = useCallback(() => setSelected((selected + 1) % _projects.length), [_projects.length, selected])
+  window.decrement = useCallback(() => setSelected((selected + _projects.length - 1) % _projects.length), [_projects.length, selected])
 
   useEffect(() => {
     getProjects().then(setProjects)
   }, [])
 
+  const projects = _projects.map((project, i) => (i === selected ? { ...project, selected: true } : project))
+
   return (
-    <div className="App">
-      <div className="project-grid">
-        {
-          projects.map(project => (
-            <div className="project">
-              <img className="icon" src={project.icon} alt="" onError={e => e.target.src =   fallback }/>
-                <div className="corner-container" >
-                  <img className="octocat" src={octocat} alt="" />
+    <div className="App"
+      style={{ background: `url(${background2})` }}
+      onKeyUp={e => {
+
+      }}
+    >
+      <div className="projects">
+        <div className="project-list">
+          <div className="projects-title">
+            Projects
+          </div>
+          <div className="project-entry-list">
+            {
+              projects.map((project, index) => (
+                <div 
+                  className={`project-entry-container ${project.selected ? "selected" : ''}`}
+                  onClick={() => setSelected(index)}
+                >
+                  <div className="line-number">
+                    {project.selected ? "> " : ""}
+                    {index + 1}
+                  </div>
+                  <div className="project-entry">
+                    {project.title}
+                  </div >
                 </div>
-                <div className="project-title">{project.title} </div>
-              </div>
-          ))
-        }
+              ))
+            }
+          </div>
+        </div>
+        <div className="project-details">
+
+        </div>
       </div>
     </div>
   );
